0b332481bcd9d6192109167cf69a817ed97394ae
Sourav Das <sourav.bt.kt@gmail.com>
2025-01-07 17:50:24 +0530

    added azure function service bus trigger boilerplate

---
+++ Boilerplates/Cloud/Azure/Functions/ServiceBusQueuerigger/ServiceBusQueueTrigger1/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+from azure.functions import ServiceBusMessage
+
+
+def main(msg: ServiceBusMessage):
+    logging.info(
+        "Python ServiceBus queue trigger processed message: %s",
+        msg.get_body().decode("utf-8"),
+    )
